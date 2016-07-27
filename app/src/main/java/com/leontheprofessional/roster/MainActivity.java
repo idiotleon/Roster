1686232c5d722fd72dab3f828290f491066f5e6f
Yang Lu (Aditi Staffing LLC) <v-ylangu@microsoft.com>
2016-07-27 16:19:06 -0700

    DialogFragment added, but not completed.

--- app/src/main/java/com/leontheprofessional/roster/MainActivity.java
+++ app/src/main/java/com/leontheprofessional/roster/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.leontheprofessional.roster.fragment.AddOnePlayerDialogFragment;
 import com.leontheprofessional.roster.fragment.ListAllPlayersFragment;
 import com.leontheprofessional.roster.fragment.ListAllWaitingListPlayersFragment;
 
@@ -73,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AddOnePlayerDialogFragment addOnePlayerDialogFragment = AddOnePlayerDialogFragment.newInstance();
+                addOnePlayerDialogFragment.show(getFragmentManager(), "dialog");
             }
         });
     }
--- app/src/main/java/com/leontheprofessional/roster/fragment/AddOnePlayerDialogFragment.java
+++ app/src/main/java/com/leontheprofessional/roster/fragment/AddOnePlayerDialogFragment.java
@@ -1,6 +1,19 @@
 package com.leontheprofessional.roster.fragment;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.leontheprofessional.roster.R;
 
 /**
  * Created by yangl on 7/5/2016.
@@ -8,4 +21,33 @@ import android.app.DialogFragment;
 public class AddOnePlayerDialogFragment extends DialogFragment{
 
 
+    public static AddOnePlayerDialogFragment newInstance(){
+        AddOnePlayerDialogFragment addOnePlayerDialogFragment = new AddOnePlayerDialogFragment();
+
+        return addOnePlayerDialogFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_one_player_dialog_fragment, container, false);
+
+        return view;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Yes")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(), "No players added", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return super.onCreateDialog(savedInstanceState);
+    }
 }
